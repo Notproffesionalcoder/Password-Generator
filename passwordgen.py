fc4702364d7391cbea446eb1ab45dc94b68e7ddd
Rishit Srivastava <87407052+Notproffesionalcoder@users.noreply.github.com>
2021-08-31 09:23:24 +0530

    Update passwordgen.py

--- passwordgen.py
+++ passwordgen.py
@@ -1,6 +1,10 @@
 import random
 import re
 letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+UPCASE_CHARACTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+					'I', 'J', 'K', 'M', 'N', 'O', 'p', 'Q',
+					'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+					'Z']
 numbers = ["1","2","3","4","5","6","7","8","9"]
 symbols = ["!","@","#","$","%","^","&","*","(",")","-","_","+","=","~"]
 passlength = input("Password length:")
